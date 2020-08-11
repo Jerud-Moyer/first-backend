08344e20615b8ff041a3ac195ad8b8bac2299100
Jerud-Moyer <jerudmoyer@gmail.com>
2020-08-11 14:42:12 -0700

    location endpoint updated

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-const geoData = require('./data/geo.js');
-const weatherData = require('./data/weather.js');
+const request = require('superagent');
+
 
 
 app.use(cors());
 app.use(express.static('public'));
+
+const {
+    GEOCODE_API_KEY,
+} = process.env;
 
 function getWeather(lat, lon) {
     const data = weatherData.data;
@@ -21,9 +25,9 @@ function getWeather(lat, lon) {
     return forecastArray;
 }
 
-function getLatLong(cityName) {
-    const city = geoData[0];
-
+async function getLatLong(cityName) {
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=38.123&lon=-78.543&key=${GEOCODE_API_KEY}&q=${cityName}&format=json`)
+    const city = response.body[0];
     return {
         formatted_query: city.display_name,
         latitude: city.lat,
@@ -32,12 +36,14 @@ function getLatLong(cityName) {
 }
 
 app.get('/location', (req, res) => {
-    const userInput = req.query.search;
-    const mungedData = getLatLong(userInput);
-    res.json(mungedData);
-    
-    
-    })
+    try {
+        const userInput = req.query.search;
+        const mungedData = await getLatLong(userInput);
+        res.json(mungedData);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+    });
 
 app.get('/weather', (req, res) => {
     try {
@@ -52,8 +58,17 @@ app.get('/weather', (req, res) => {
     }
         
     });
+
+    app.get('/Movies', async(req, res) => {
+        try
+        const userInput = req.query.search;
+        const mungedData = getLatLong(userInput);
+        res.json(mungedData);
+        
+        
+        });
   
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
