59db4db6edd8a429ecf6c5efedfe07c7298f5793
Jerud-Moyer <jerudmoyer@gmail.com>
2020-08-11 15:52:50 -0700

    movies working

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.static('public'));
 
 const {
     GEOCODE_API_KEY,
+    MOVIE_API_KEY,
 } = process.env;
 
 function getWeather(lat, lon) {
@@ -26,13 +27,25 @@ function getWeather(lat, lon) {
 }
 
 async function getLatLong(cityName) {
-    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`)
+    const response = await request.get(`https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${cityName}&format=json`);
     const city = response.body[0];
     return {
         formatted_query: city.display_name,
         latitude: city.lat,
         longitude: city.lon,
     };
+}
+
+async function mungeMovies(cityName) {
+    const data = await request.get(`http://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityName}`);
+    const movies = data.body.results;
+    const mungedMovies = movies.map((movie) => {
+        return {
+            title: movie.original_title,
+            release: movie.release_date
+        };
+    });
+    return mungedMovies;
 }
 
 app.get('/location', async(req, res) => {
@@ -62,11 +75,13 @@ app.get('/weather', (req, res) => {
     app.get('/Movies', async(req, res) => {
         try {
         const userInput = req.query.search;
-        const mungedData = getLatLong(userInput);
-        res.json(mungedData);
-        } catch {}
+        const movies = await mungeMovies(userInput);
+        res.json(movies);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
         
-        });
+    });
   
 
 app.listen(port, () => {
